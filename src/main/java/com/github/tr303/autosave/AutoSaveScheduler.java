84008cc81c707f1b48426ccfc0170627d578c999
CXY <2867747371@qq.com>
2024-10-07 01:54:42 +0800

    Component is deprecated. Change scheduler into a service.

--- src/main/java/com/github/tr303/autosave/AutoSaveScheduler.java
+++ src/main/java/com/github/tr303/autosave/AutoSaveScheduler.java
@@ -4,7 +4,6 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -13,14 +12,13 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
 
-public class AutoSaveScheduler implements ApplicationComponent {
+public class AutoSaveScheduler implements AutoCloseable {
     private ScheduledExecutorService scheduler;
     private boolean isRunning = false; // 状态标识符，表示任务是否正在运行
 
-    @Override
-    public void initComponent() {
-        System.out.println("## construct.");
-        startScheduler(); // 在组件初始化时启动任务
+    // 构造函数，在服务初始化时启动调度任务
+    public AutoSaveScheduler() {
+        startScheduler(); // 在服务初始化时启动任务
     }
 
     // 启动定时任务的方法
@@ -32,7 +30,7 @@ public class AutoSaveScheduler implements ApplicationComponent {
 
         scheduler = Executors.newScheduledThreadPool(1);
 
-        // 每五秒调用一次指定函数
+        // 每 30 秒调用一次 autoSave() 函数
         scheduler.scheduleAtFixedRate(this::autoSave, 0, 30, TimeUnit.SECONDS);
         isRunning = true; // 更新状态为运行中
         System.out.println("Scheduler started.");
@@ -72,12 +70,12 @@ public class AutoSaveScheduler implements ApplicationComponent {
             Notification notification = new Notification("AutoSaveNotifications", "Saving current version", "Please wait...", NotificationType.INFORMATION);
             Notifications.Bus.notify(notification, project);
 
-            Boolean result = new AutoSaveFunctional(project).saveCurrentProjectAsVersion("User Quick Saved");
+            Boolean result = new AutoSaveFunctional(project).saveCurrentProjectAsVersion("30'' Auto Save ");
 
             if (result != null && result) {
                 notification.setContent("Succeed！");
             } else {
-                notification.setContent("Failed！There is nothing to save");
+                notification.setContent("No change detected！There is nothing to save");
             }
 
             Notifications.Bus.notify(notification, project);
@@ -85,8 +83,7 @@ public class AutoSaveScheduler implements ApplicationComponent {
     }
 
     @Override
-    public void disposeComponent() {
-        // 组件被销毁时停止调度器
-        stopScheduler();
+    public void close() {
+        stopScheduler(); // 调用关闭方法时停止调度任务
     }
 }
