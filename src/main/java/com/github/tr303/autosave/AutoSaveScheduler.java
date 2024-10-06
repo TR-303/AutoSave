aca67366968fbc6b83737ed757ef140ab5e65b02
CXY <2867747371@qq.com>
2024-10-07 01:31:23 +0800

    save a version every 30 seconds

--- src/main/java/com/github/tr303/autosave/AutoSaveScheduler.java
+++ src/main/java/com/github/tr303/autosave/AutoSaveScheduler.java
@@ -1,6 +1,13 @@
 package com.github.tr303.autosave;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +33,7 @@ public class AutoSaveScheduler implements ApplicationComponent {
         scheduler = Executors.newScheduledThreadPool(1);
 
         // 每五秒调用一次指定函数
-        scheduler.scheduleAtFixedRate(this::myScheduledFunction, 0, 5, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(this::autoSave, 0, 30, TimeUnit.SECONDS);
         isRunning = true; // 更新状态为运行中
         System.out.println("Scheduler started.");
     }
@@ -46,9 +53,35 @@ public class AutoSaveScheduler implements ApplicationComponent {
     }
 
     // 定时任务的执行函数
-    private void myScheduledFunction() {
-        System.out.println("Executing scheduled function...");
-        // 在这里实现你的函数逻辑
+    private void autoSave() {
+        Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+        if (openProjects.length == 0) {
+            System.out.println("No open projects.");
+            return;
+        }
+        System.out.println("########### Auto Save ###########");
+
+        // 确保保存操作在 Event Dispatch Thread (EDT) 中同步执行
+        ApplicationManager.getApplication().invokeAndWait(() -> {
+            FileDocumentManager.getInstance().saveAllDocuments();
+        });
+
+        for (Project project : openProjects) {
+            if (project == null) continue;// 似乎不会发生？
+
+            Notification notification = new Notification("AutoSaveNotifications", "Saving current version", "Please wait...", NotificationType.INFORMATION);
+            Notifications.Bus.notify(notification, project);
+
+            Boolean result = new AutoSaveFunctional(project).saveCurrentProjectAsVersion("User Quick Saved");
+
+            if (result != null && result) {
+                notification.setContent("Succeed！");
+            } else {
+                notification.setContent("Failed！There is nothing to save");
+            }
+
+            Notifications.Bus.notify(notification, project);
+        }
     }
 
     @Override
